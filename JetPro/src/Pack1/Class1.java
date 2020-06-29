3522e5295b5f27a732f088d5cda9911aad3444b7
fahad <myjira17@gmail.com>
2020-06-28 23:48:12 -0700

    Committed by fahad

---
+++ JetPro/src/Pack1/Class1.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Class1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.gecko.driver","D:\\Desktop and Download Data\\Desktop\\Selenium\\Gecko Driver\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
